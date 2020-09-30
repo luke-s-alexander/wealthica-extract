df605faeb8ac65a4db8c2411b2345fc68fea276d
Luke Alexander <lalexander@Lukes-MBP-2.ht.home>
2020-09-30 19:54:59 -0400

    Add sorting function to test.js. Sorted properties are currently hard-coded

--- src/sort.js
+++ src/sort.js
@@ -38,6 +38,12 @@ function sortCsv (csvStr) {
 			objectRowsArray[index -1] = rowObject;
 		};
 	});
+	var sortKeys = ['account', 'class', 'symbol'];
+
+	objectRowsArray.sort(dynamicSortMultiple("account","-class","symbol"));
+
+	// console.log(objectRowsArray[0].account);
+	// objectRowsArray.sort((a,b) => (a.account > b.account) ? 1 : ((b.account > a.account) ? -1 : 0));
 	console.log(objectRowsArray);
 
 
@@ -55,5 +61,40 @@ function sortCsv (csvStr) {
 // return sorted csv string
 
 };
+
+function dynamicSortMultiple() {
+    /*
+     * save the arguments object as it will be overwritten
+     * note that arguments object is an array-like object
+     * consisting of the names of the properties to sort by
+     */
+    var props = arguments;
+    return function (obj1, obj2) {
+        var i = 0, result = 0, numberOfProperties = props.length;
+        /* try getting a different result from 0 (equal)
+         * as long as we have extra properties to compare
+         */
+        while(result === 0 && i < numberOfProperties) {
+            result = dynamicSort(props[i])(obj1, obj2);
+            i++;
+        }
+        return result;
+    }
+}
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        /* next line works with strings and numbers, 
+         * and you may want to customize it to your needs
+         */
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
 
 
--- src/test.js
+++ src/test.js
@@ -295,7 +295,8 @@ $(function () {
 	        });
       });
       csvStr = csvStr.concat(cashCsv);
-      return encodeURIComponent(csvStr);
+      sortedCsvStr = sortCsv(csvStr);
+      return encodeURIComponent(sortedCsvStr);
     } catch (error) { 
       console.log(error)
     }
@@ -505,7 +506,7 @@ $(function () {
       });
       csvStr += lineDelimiter
     });
-   return encodeURIComponent(csvStr);
+    return encodeURIComponent(csvStr);
   };
   // Parse Institutions JSON object into CSV string
   function exportCashCustomToCsvFile(jsonData) {
@@ -585,4 +586,63 @@ $(function () {
       linkElement.click();
   };
 
+  function sortCsv (csvStr) { 
+    // Capture the keys from header row
+    csvRowsArray = csvStr.split('\n');
+    keys = csvRowsArray[0].split(',');
+    
+    // Build an array of rows as objects 
+    objectRowsArray = [];
+    csvRowsArray.forEach((element, index) => {
+      // Skip header row (index=0) and blank rows
+      if (index > 0 && csvRowsArray[index] != '') {
+        // Create array for row
+        var row = csvRowsArray[index].split(',');
+        // Create object to be populated with data from row array
+        var rowObject = {};
+        // Create object properties from keys and populate with row array data
+        keys.forEach((key, index) => { 
+          rowObject[key] = row[index];
+        });
+        // Index=0 is skipped (it is header row)
+        objectRowsArray[index -1] = rowObject;
+      };
+    });
+    return objectRowsArray.sort(dynamicSortMultiple("account","-class","symbol"));
+  };
+
+  function dynamicSortMultiple() {
+    /*
+     * save the arguments object as it will be overwritten
+     * note that arguments object is an array-like object
+     * consisting of the names of the properties to sort by
+     */
+    var props = arguments;
+    return function (obj1, obj2) {
+        var i = 0, result = 0, numberOfProperties = props.length;
+        /* try getting a different result from 0 (equal)
+         * as long as we have extra properties to compare
+         */
+        while(result === 0 && i < numberOfProperties) {
+            result = dynamicSort(props[i])(obj1, obj2);
+            i++;
+        }
+        return result;
+    }
+  };
+
+  function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        /* next line works with strings and numbers, 
+         * and you may want to customize it to your needs
+         */
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+  };  
 });
